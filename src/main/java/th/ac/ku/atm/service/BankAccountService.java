50a3caef34e1879278b78999c8b5d80d36b3f4c8
Sittichok Hongthong <sittichok.ho@gmail.com>
2021-02-20 19:34:02 +0700

    .

--- src/main/java/th/ac/ku/atm/service/BankAccountService.java
+++ src/main/java/th/ac/ku/atm/service/BankAccountService.java
@@ -2,6 +2,7 @@ package th.ac.ku.atm.service;
 
 import org.springframework.stereotype.Service;
 import th.ac.ku.atm.model.BankAccount;
+import th.ac.ku.atm.model.Customer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,10 +13,23 @@ public class BankAccountService {
     private ArrayList<BankAccount> bankAccountsList = new ArrayList<>();
 
     public void createAccount(BankAccount bankAccount){
-        bankAccountsList.add(bankAccount);
+        BankAccount storedBankAccount = findBankAccount(bankAccount.getId());
+        if(storedBankAccount == null){
+            bankAccountsList.add(bankAccount);
+        }
+
     }
 
     public List<BankAccount> getAccount(){
+
         return new ArrayList<>(bankAccountsList);
+    }
+
+    public BankAccount findBankAccount(int id) {
+        for (BankAccount account : bankAccountsList) {
+            if (account.getId() == id)
+                return account;
+        }
+        return null;
     }
 }
